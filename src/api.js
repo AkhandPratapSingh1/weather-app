784eb84fda460bcd90e68e6e2a6ec0da92ba7da2
AkhandPratapSingh1 <akhandpratap6397@gmail.com>
2023-07-15 10:50:09 +0530

    First Upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '932f2e6e5amsh512f0067d620c67p1e4238jsn9eff526bbcc7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'ef98c012e4f8d0535e0c68978c21d7d0';
